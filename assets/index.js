b83e7035981c5a871ee5835f18001cd893237398
fkakatie <fkakatie@gmail.com>
2021-02-03 22:11:12 -0700

    update index responsiveness

--- assets/index.js
+++ assets/index.js
@@ -11,19 +11,20 @@ const setPage = () => {
     // console.log("merch");
   } else if (path.includes("merch")) {
     // console.log("pint-club");
-  } else { // default location is store
+  } else {
+    // default location is store
     // console.log("store");
     buildIndexGrid();
     // horizontalScroll();
   }
-}
+};
 
 const classify = () => {
   // add classes/ids based on inner text of headings
   const $h1s = document.querySelectorAll("h1");
   const $h2s = document.querySelectorAll("h2");
   const $h3s = document.querySelectorAll("h3");
-  const $headings = [ $h1s, $h2s, $h3s ];
+  const $headings = [$h1s, $h2s, $h3s];
 
   $headings.forEach((headings) => {
     if (headings) {
@@ -32,39 +33,54 @@ const classify = () => {
         const text = cleanName(h.textContent);
         h.setAttribute("class", text); // attach class to heading
         h.parentNode.setAttribute("id", text); // attach id to parent
-      })
+      });
     }
-  })
-  
-}
+  });
+};
 
 const cleanName = (str) => {
-  const clean = str.split(" ").join("") // remove spaces
+  const clean = str
+    .split(" ")
+    .join("") // remove spaces
     .toLowerCase()
-    .replace(/[^0-9a-z]/gi, ''); // replace non alpha-numeric
+    .replace(/[^0-9a-z]/gi, ""); // replace non alpha-numeric
   return clean;
-}
+};
 
 const buildIndexGrid = () => {
   // console.log(`\nbuild index grid running`);
-  const indexPaths = [ "/", "/index", "/index.html" ];
+  const indexPaths = ["/", "/index", "/index.html"];
   if (indexPaths.includes(window.location.pathname)) {
     const $main = document.querySelector("main");
-    const carouselHeight = parseInt(document.querySelector("#carousel").offsetHeight);
+    const carouselHeight = parseInt(
+      document.querySelector("#carousel").offsetHeight
+    );
     $main.classList.add("flex");
-    $main.style.minHeight = `${carouselHeight > 0 ? carouselHeight + 32 : 697}px`;
+    $main.style.minHeight = `${
+      carouselHeight > 0 ? carouselHeight + 32 : 697
+    }px`;
+    const $carousel = document.querySelector(".embed-internal-carousel");
+    if ($carousel) { // set horizonal scroll on carousel
+      $carousel.onwheel = (e) => {
+        if (e.wheelDelta < 0) {
+          document.querySelector(".embed-internal-carousel").scrollLeft += 468;
+        } else {
+          document.querySelector(".embed-internal-carousel").scrollLeft -= 468;
+        }
+      };
+    }
   }
-}
+};
 
 const updateCopyright = () => {
   // update copyright year in footer
-  const date = new Date; 
+  const date = new Date();
   const year = date.getFullYear();
   const $footer = document.querySelector("footer > div");
   if (year > 2021) {
     $footer.textContent += ` - ${year}`;
   }
-}
+};
 
 // const horizontalScroll = () => {
 //   const $carousel = document.querySelector(".embed-internal-carousel");
